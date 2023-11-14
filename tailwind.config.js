ef6daceea9dde61de9b19735e8aec76dac03dee7
yashsharma110 <yashsharma5312@gmail.com>
2023-11-14 18:07:42 +0530

    added tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      lg: { max: "850px" },
+      md: { max: "620px" },
+      sm: { max: "440px" },
+      xs: { max: "400px" },
+    },
+    extend: {
+      fontFamily: {
+        coverge: ["Convergence", "sans-serif"],
+      },
+      colors: {
+        brownRed: "#7d6262",
+        brownRedHover: "#ab8686",
+        lightRed: "#ffe1e1",
+        lightRedHover: "#f5c8c8",
+      },
+    },
+  },
+  plugins: [],
+};
