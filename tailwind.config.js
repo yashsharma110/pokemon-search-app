94873d7eadb95b3bec451aa0571ad7d6766407f3
yashsharma110 <yashsharma5312@gmail.com>
2023-11-16 17:01:58 +0530

    modified tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
       lg: { max: "850px" },
